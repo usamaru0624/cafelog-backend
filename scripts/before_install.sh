4ab13840b5f0d2a127765b6fa829a4a37a7e7000
EC2 Default User <ec2-user@ip-10-0-1-154.ap-northeast-1.compute.internal>
2025-05-14 06:02:29 +0000

    fix: add chown to resolve permission error

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "==== before_install: cleaning deploy directory ===="
 sudo rm -f /home/ec2-user/cafelog-deploy/cafelog-0.0.1-SNAPSHOT.jar
-
-
+sudo chown -R ec2-user:ec2-user /home/ec2-user/cafelog-deploy
